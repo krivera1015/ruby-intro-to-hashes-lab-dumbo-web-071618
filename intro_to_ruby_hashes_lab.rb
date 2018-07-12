7dc63973afb52691a3bee7150bbaa1188165adcf
Kevin  Rivera <krivera1015@gmail.com>
2018-07-12 21:22:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,12 +7,15 @@ def actor
 end
 
 def monopoly
-	monopoly = {railroads: {} 
+	monopoly = {railroads: {}
 	}
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {} 
+	monopoly = {railroads: {
+		pieces: 4
+		name:{}
+	},
 	}
 end
 
